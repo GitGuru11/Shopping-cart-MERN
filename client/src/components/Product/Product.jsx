b0b9c2d93e47682d314ac0563da0b4fde61b3f48
GitGuru11 <topdev0101@gmail.com>
2025-02-13 11:49:08 +0200

    add: added one item

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Product.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Product = props => {
+const Product = (props) => {
+  const [item, setItem] = useState({});
+
   return (
     <div className="Product-Wrapper">
-    <div className="Product">
+      <div className="Product">
         <div className="Product-Image-Wrapper">
           <img src={props.photo} alt={props.name} className="Product-Image" />
         </div>
-      <div className="Product-Title">
-        <p className="Product-Name">{props.name}</p>
-      </div>
+        <div className="Product-Title">
+          <p className="Product-Name">{props.name}</p>
+        </div>
         <div className="Product-Data">
           <div>Products</div>
-        <small className="Product-Price">${props.price}</small>
-        <button onClick={ props.addToCart } className="product-button Product-Add">Add to Cart</button>
+          <small className="Product-Price">${props.price}</small>
+          <button
+            onClick={props.addToCart}
+            className="product-button Product-Add"
+          >
+            Add to Cart
+          </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
@@ -26,7 +33,7 @@ Product.propTypes = {
   name: PropTypes.string.isRequired,
   photo: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  addToCart: PropTypes.func.isRequired
+  addToCart: PropTypes.func.isRequired,
 };
 
 export default Product;
